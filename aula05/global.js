ddbbeaaa8f8efe015e25e509ab9e2e4f5a0f6466
dev-edsonlopes <dev_edson@outlook.com>
2023-04-27 10:17:04 -0300

    global node == Window Browser

---
+++ aula05/global.js
@@ -0,0 +1,8 @@
+// console.log(global)
+
+global.MeuApp = Object.freeze({
+    saudacao() {
+        return 'Este é um sistema acessado  para todos'
+    },
+    nome: 'Sistema exemplo'
+})
---
+++ aula05/globalCliente.js
@@ -0,0 +1,4 @@
+require('./global')
+
+console.log('Nome do Sistem:', MeuApp.nome)
+console.log('Descrioção:',MeuApp.saudacao())
